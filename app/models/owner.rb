9d50b7c023f807015817c0a4a8070f9e67a37c12
Nikolaos Alexiou <nikolaos.alexiou@gmail.com>
2014-10-21 09:05:52 -0400

    added age to the model and view

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -1,18 +1,33 @@
 class Owner
 
+
 	def name
-		name = 'Foobar Kadigan'
+		name = 'Nikolaos'
 	end
 	def birthdate
-		birthdate = Date.new(1990, 12, 22)
+		birthdate = Date.new(1978, 11, 25)
 	end
-	def countdown
-		today = Date.today
+
+	def birthday
 		birthday = Date.new(today.year, birthdate.month, birthdate.day) 
+	end
+
+	def today
+		today = Date.today
+	end
+
+	def countdown
 		if birthday > today
 			countdown = (birthday - today).to_i
 		else
 			countdown = (birthday.next_year - today).to_i
+		end 
+	end
+	def age
+		if birthday < today
+			age = (today.year - birthdate.year).to_i
+		else
+			age = (today.year - birthdate.year-1).to_i
 		end 
 	end
 end
